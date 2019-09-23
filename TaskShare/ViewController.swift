f62a1943157f381b41fb1b654d62837e2736730e
tomoyasuzuki <tomoya.s0121@icloud.com>
2019-09-23 23:32:32 +0900

    Initial Commit

---
+++ TaskShare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskShare
+//
+//  Created by 鈴木友也 on 2019/09/23.
+//  Copyright © 2019 tomoya.suzuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TaskShareTests/TaskShareTests.swift
@@ -0,0 +1,34 @@
+//
+//  TaskShareTests.swift
+//  TaskShareTests
+//
+//  Created by 鈴木友也 on 2019/09/23.
+//  Copyright © 2019 tomoya.suzuki. All rights reserved.
+//
+
+import XCTest
+@testable import TaskShare
+
+class TaskShareTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
